81670baa8e44f4ff7f9c0b233b212dabad4259b0
manpreet.bagga <manpreet.bagga@C1-BF132.india.rsystems.com>
2017-07-11 18:48:15 +0530

    update

---
+++ updater/main.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const updater = require('updater');
+const path = require('path');
+let win
+function createWindow() {
+   win = new BrowserWindow({width: 800, height: 600})
+   win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }))
+}
+
+app.on('ready', createWindow)
